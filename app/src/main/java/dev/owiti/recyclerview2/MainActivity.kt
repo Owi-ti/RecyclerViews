d90879c94981c14247f1c5e530eb67bc9e173e51
Owi-ti <owittiesther@gmail.com>
2022-07-04 07:49:46 +0300

    chagnes

--- app/src/main/java/dev/owiti/recyclerview2/MainActivity.kt
+++ app/src/main/java/dev/owiti/recyclerview2/MainActivity.kt
@@ -7,30 +7,38 @@ class MainActivity : AppCompatActivity() {
     lateinit var binding:ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMainBinding.inflate(layoutInflater)
+        binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         fibonacciNums()
+
+
     }
     fun fibonacciNums(){
-        val k = 100
-        var no1 = 0
-        var no2 = 1
-        var x = mutableListOf<Int>()
+        var numberList=fabonacciNumbers(100)
+        var numberAdapter = NumberRecyclerViewAdapter(numberList)
+        binding.rvNum.layoutManager = LinearLayoutManager(this)
+        binding.rvNum.adapter = numberAdapter
 
-        println(x)
-
-        for (i in 1..100) {
-            x.add(no1)
-            var sum = no1 + no2
-            no1 = no2
-            no2 = sum
-        }
-
-        var numberAdapter = NumberRecyclerViewAdapter(x)
-        binding.rvNum.layoutManager= LinearLayoutManager(this)
-        binding.rvNum.adapter=numberAdapter
 
 
     }
+    fun fabonacciNumbers(size:Int):List<Int>{
+        var numberList=ArrayList<Int>()
+        var no1 = 0
+        var no2 = 1
+        var number=0
+        while (number<=size){
+            print(no1)
+            var sum=no1+no2
+            no1=no2
+            no2=sum
+            number++
+            numberList+=sum
 
+        }
+        return numberList
+
+
+
+    }
 }
--- app/src/main/java/dev/owiti/recyclerview2/NumberRecyclerViewAdatper.kt
+++ app/src/main/java/dev/owiti/recyclerview2/NumberRecyclerViewAdatper.kt
@@ -3,23 +3,29 @@ package dev.owiti.recyclerview2
 import ...
 
 
-class NumberRecyclerViewAdapter(var Numbers:List<Int>):RecyclerView.Adapter<NumberViewHolder>() {
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
-        var View = LayoutInflater.from(parent.context)
-            .inflate(R.layout.number_list_item, parent, false)
-        return  NumberViewHolder(View)
 
+class NumberRecyclerViewAdapter(var numberList: List<Int>):RecyclerView.Adapter<NumberRecyclerViewAdapter.NumberViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NumberViewHolder {
+        var itemView =
+            LayoutInflater.from(parent.context).inflate(R.layout.number_list_item, parent, false)
+        return NumberViewHolder(itemView)
     }
 
+
     override fun onBindViewHolder(holder: NumberViewHolder, position: Int) {
-        holder.tvNum.inputType=Numbers.get(position)
+
+        holder.tvNum.text = numberList.get(position).toString()
     }
 
     override fun getItemCount(): Int {
-        return Numbers.size
+        return numberList.size
     }
-}
-class NumberViewHolder(itemView: View):RecyclerView.ViewHolder(itemView){
-    var tvNum=itemView.findViewById<TextView>(R.id.tvNum)
+
+    class NumberViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+        var tvNum= itemView.findViewById<TextView>(R.id.tvNum)
+
+    }
 
 }
+
+
